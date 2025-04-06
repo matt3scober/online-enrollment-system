6ac6a75e8d6c8210030d2be3e398609db7750438
mattescober <145801129+matt3scober@users.noreply.github.com>
2025-04-06 16:58:51 +0800

    5 spring boot application
    all 5 spring boot applications were made through spring initializr

---
+++ course-service/src/test/java/com/p4/stdiscm/course_service/CourseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.p4.stdiscm.course_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ grading-service/src/test/java/com/p4/stdiscm/grading_service/GradingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.p4.stdiscm.grading_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
