ffa8fdbd139631b37a50ba3a33e5e38be47b3301
mattescober <145801129+matt3scober@users.noreply.github.com>
2025-04-06 16:24:33 +0800

    initial spring boot
    a

---
+++ src/test/java/com/p4/stdiscm/p4_stdiscm/P4StdiscmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.p4.stdiscm.p4_stdiscm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P4StdiscmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
