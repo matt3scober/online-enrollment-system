dd9816cc41d3d00b36eb889e3662bbf11041d741
mattescober <145801129+matt3scober@users.noreply.github.com>
2025-04-12 12:58:17 +0800

    Success Run App

--- auth-service/src/main/java/com/p4/stdiscm/auth_service/AuthServiceApplication.java
+++ auth-service/src/main/java/com/p4/stdiscm/auth_service/AuthServiceApplication.java
@@ -2,8 +2,8 @@ package com.p4.stdiscm.auth_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class AuthServiceApplication {
@@ -12,8 +12,8 @@ public class AuthServiceApplication {
         SpringApplication.run(AuthServiceApplication.class, args);
     }
     
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+    // @Bean
+    // public BCryptPasswordEncoder passwordEncoder() {
+    //     return new BCryptPasswordEncoder();
+    // }
 }
